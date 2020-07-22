b6b348ede3d768924b7e297b05b9b28fba23435a
杨挺 <1062565969@qq.com>
2020-07-22 10:07:40 +0800

    update

--- Pods/Target Support Files/Pods-Demo3000/Pods-Demo3000-resources.sh
+++ Pods/Target Support Files/Pods-Demo3000/Pods-Demo3000-resources.sh
@@ -97,11 +97,9 @@ EOM
   esac
 }
 if [[ "$CONFIGURATION" == "Debug" ]]; then
-  install_resource "${PODS_ROOT}/VLionAdSDKPoly/VLionAdSDKPoly/RSGameVlionAd/RSGameVlionAd.bundle"
   install_resource "${PODS_ROOT}/VLionAdSDKPoly/VLionAdSDKPoly/VLionADSDK/VLionAdImg.bundle"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
-  install_resource "${PODS_ROOT}/VLionAdSDKPoly/VLionAdSDKPoly/RSGameVlionAd/RSGameVlionAd.bundle"
   install_resource "${PODS_ROOT}/VLionAdSDKPoly/VLionAdSDKPoly/VLionADSDK/VLionAdImg.bundle"
 fi
 
